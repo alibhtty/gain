fbeca9054c15d7b2be32805ad292abf035dfc884
alibhtty <77213825+alibhtty@users.noreply.github.com>
2023-12-05 10:22:09 +0100

    Actualizar data.js

--- data.js
+++ data.js
@@ -37,14 +37,14 @@ var diasDelAño = [
     /* DICIEMBRE */
     { dia: 30, nivel: 3 },
     { dia: 1, nivel: 3 },
-    { dia: 2, nivel: 0 },
+    { dia: 2, nivel: 4 },
     { dia: 3, nivel: 4 }, /* DOM */
     { dia: 4, nivel: 2 },
     { dia: 5, nivel: 0 },
     { dia: 6, nivel: 0 },
     { dia: 7, nivel: 0 },
     { dia: 8, nivel: 0 },
-    { dia: 9, nivel: 0 },
+    { dia: 9, nivel: 4 },
 
     { dia: 10, nivel: 4 }, /* DOM */
     { dia: 11, nivel: 0 },
@@ -52,7 +52,7 @@ var diasDelAño = [
     { dia: 13, nivel: 0 },
     { dia: 14, nivel: 0 },
     { dia: 15, nivel: 0 },
-    { dia: 16, nivel: 0 },
+    { dia: 16, nivel: 4 },
     { dia: 17, nivel: 4 }, /* DOM */
     { dia: 18, nivel: 0 },
     { dia: 19, nivel: 0 },
@@ -60,7 +60,7 @@ var diasDelAño = [
     { dia: 20, nivel: 0 },
     { dia: 21, nivel: 0 },
     { dia: 22, nivel: 0 },
-    { dia: 23, nivel: 0 },
+    { dia: 23, nivel: 4 },
     { dia: 24, nivel: 4 }, /* DOM */
     { dia: 25, nivel: 0 },
     { dia: 26, nivel: 0 },
@@ -69,14 +69,14 @@ var diasDelAño = [
     { dia: 29, nivel: 0 },
 
     /* ENERO */
-    { dia: 30, nivel: 0 },
+    { dia: 30, nivel: 4 },
     { dia: 31, nivel: 4 }, /* DOM */
     { dia: 1, nivel: 0 },
     { dia: 2, nivel: 0 },
     { dia: 3, nivel: 0 },
     { dia: 4, nivel: 0 },
     { dia: 5, nivel: 0 },
-    { dia: 6, nivel: 0 },
+    { dia: 6, nivel: 4 },
     { dia: 7, nivel: 4 }, /* DOM */
     { dia: 8, nivel: 0 },
     { dia: 9, nivel: 0 },
@@ -84,7 +84,7 @@ var diasDelAño = [
     { dia: 10, nivel: 0 },
     { dia: 11, nivel: 0 },
     { dia: 12, nivel: 0 },
-    { dia: 13, nivel: 0 },
+    { dia: 13, nivel: 4 },
     { dia: 14, nivel: 4 }, /* DOM */
     { dia: 15, nivel: 0 },
     { dia: 16, nivel: 0 },
@@ -92,14 +92,14 @@ var diasDelAño = [
     { dia: 18, nivel: 0 },
     { dia: 19, nivel: 0 },
     
-    { dia: 20, nivel: 0 },
+    { dia: 20, nivel: 4 },
     { dia: 21, nivel: 4 }, /* DOM */
     { dia: 22, nivel: 0 },
     { dia: 23, nivel: 0 },
     { dia: 24, nivel: 0 },
     { dia: 25, nivel: 0 },
     { dia: 26, nivel: 0 },
-    { dia: 27, nivel: 0 },
+    { dia: 27, nivel: 4 },
     { dia: 28, nivel: 4 }, /* DOM */
     { dia: 29, nivel: 0 },
 
@@ -108,7 +108,7 @@ var diasDelAño = [
     { dia: 31, nivel: 0 },
     { dia: 1, nivel: 0 },
     { dia: 2, nivel: 0 },
-    { dia: 3, nivel: 0 },
+    { dia: 3, nivel: 4 },
     { dia: 4, nivel: 4 }, /* DOM */
     { dia: 5, nivel: 0 },
     { dia: 6, nivel: 0 },
@@ -116,14 +116,14 @@ var diasDelAño = [
     { dia: 8, nivel: 0 },
     { dia: 9, nivel: 0 },
 
-    { dia: 10, nivel: 0 },
+    { dia: 10, nivel: 4 },
     { dia: 11, nivel: 4 }, /* DOM */
     { dia: 12, nivel: 0 },
     { dia: 13, nivel: 0 },
     { dia: 14, nivel: 0 },
     { dia: 15, nivel: 0 },
     { dia: 16, nivel: 0 },
-    { dia: 17, nivel: 0 },
+    { dia: 17, nivel: 4 },
     { dia: 18, nivel: 4 }, /* DOM */
 
     { dia: 19, nivel: 0 },
@@ -131,7 +131,7 @@ var diasDelAño = [
     { dia: 21, nivel: 0 },
     { dia: 22, nivel: 0 },
     { dia: 23, nivel: 0 },
-    { dia: 24, nivel: 0 },
+    { dia: 24, nivel: 4 },
     { dia: 25, nivel: 4 }, /* DOM */
     { dia: 26, nivel: 0 },
     { dia: 27, nivel: 0 },
@@ -140,14 +140,14 @@ var diasDelAño = [
     /* MARZO */
     { dia: 29, nivel: 0 },
     { dia: 1, nivel: 0 },
-    { dia: 122, nivel: 0 },
+    { dia: 122, nivel: 4 },
     { dia: 123, nivel: 4 }, /* DOM */
     { dia: 124, nivel: 0 },
     { dia: 125, nivel: 0 },
     { dia: 126, nivel: 0 },
     { dia: 127, nivel: 0 },
     { dia: 128, nivel: 0 },
-    { dia: 129, nivel: 0 },
+    { dia: 129, nivel: 4 },
 
     { dia: 130, nivel: 4 }, /* DOM */
     { dia: 131, nivel: 0 },
@@ -163,7 +163,7 @@ var diasDelAño = [
     { dia: 140, nivel: 0 },
     { dia: 141, nivel: 0 },
     { dia: 142, nivel: 0 },
-    { dia: 143, nivel: 0 },
+    { dia: 143, nivel: 4 },
     { dia: 144, nivel: 4 },
     { dia: 145, nivel: 0 },
     { dia: 146, nivel: 0 },
@@ -172,14 +172,14 @@ var diasDelAño = [
     { dia: 149, nivel: 0 },
 
     /* ABRIL */
-    { dia: 150, nivel: 0 },
+    { dia: 150, nivel: 4 },
     { dia: 151, nivel: 4 },
     { dia: 152, nivel: 0 },
     { dia: 153, nivel: 0 },
     { dia: 154, nivel: 0 },
     { dia: 155, nivel: 0 },
     { dia: 156, nivel: 0 },
-    { dia: 157, nivel: 0 },
+    { dia: 157, nivel: 4 },
     { dia: 158, nivel: 4 },
     { dia: 159, nivel: 0 },
 
@@ -187,7 +187,7 @@ var diasDelAño = [
     { dia: 161, nivel: 0 },
     { dia: 162, nivel: 0 },
     { dia: 163, nivel: 0 },
-    { dia: 164, nivel: 0 },
+    { dia: 164, nivel: 4 },
     { dia: 165, nivel: 4 },
     { dia: 166, nivel: 0 },
     { dia: 167, nivel: 0 },
@@ -195,14 +195,14 @@ var diasDelAño = [
     { dia: 169, nivel: 0 },
     
     { dia: 170, nivel: 0 },
-    { dia: 171, nivel: 0 },
+    { dia: 171, nivel: 4 },
     { dia: 172, nivel: 4 },
     { dia: 173, nivel: 0 },
     { dia: 174, nivel: 0 },
     { dia: 175, nivel: 0 },
     { dia: 176, nivel: 0 },
     { dia: 177, nivel: 0 },
-    { dia: 178, nivel: 0 },
+    { dia: 178, nivel: 4 },
     { dia: 179, nivel: 4 },
 
     /* MAYO */
@@ -211,7 +211,7 @@ var diasDelAño = [
     { dia: 182, nivel: 0 },
     { dia: 183, nivel: 0 },
     { dia: 184, nivel: 0 },
-    { dia: 185, nivel: 0 },
+    { dia: 185, nivel: 4 },
     { dia: 186, nivel: 4 },
     { dia: 187, nivel: 0 },
     { dia: 188, nivel: 0 },
@@ -219,14 +219,14 @@ var diasDelAño = [
 
     { dia: 190, nivel: 0 },
     { dia: 191, nivel: 0 },
-    { dia: 192, nivel: 0 },
+    { dia: 192, nivel: 4 },
     { dia: 193, nivel: 4 },
     { dia: 194, nivel: 0 },
     { dia: 195, nivel: 0 },
     { dia: 196, nivel: 0 },
     { dia: 197, nivel: 0 },
     { dia: 198, nivel: 0 },
-    { dia: 199, nivel: 0 },
+    { dia: 199, nivel: 4 },
     
     { dia: 200, nivel: 4 },
     { dia: 201, nivel: 0 },
@@ -234,7 +234,7 @@ var diasDelAño = [
     { dia: 203, nivel: 0 },
     { dia: 204, nivel: 0 },
     { dia: 205, nivel: 0 },
-    { dia: 206, nivel: 0 },
+    { dia: 206, nivel: 4 },
     { dia: 207, nivel: 4 },
     { dia: 208, nivel: 0 },
     { dia: 209, nivel: 0 },
@@ -243,7 +243,7 @@ var diasDelAño = [
     { dia: 210, nivel: 0 },
     { dia: 211, nivel: 0 },
     { dia: 212, nivel: 0 },
-    { dia: 213, nivel: 0 },
+    { dia: 213, nivel: 4 },
     { dia: 214, nivel: 4 },
     { dia: 215, nivel: 0 },
     { dia: 216, nivel: 0 },
@@ -251,14 +251,14 @@ var diasDelAño = [
     { dia: 218, nivel: 0 },
     { dia: 219, nivel: 0 },
 
-    { dia: 220, nivel: 0 },
+    { dia: 220, nivel: 4 },
     { dia: 221, nivel: 4 },
     { dia: 222, nivel: 0 },
     { dia: 223, nivel: 0 },
     { dia: 224, nivel: 0 },
     { dia: 225, nivel: 0 },
     { dia: 226, nivel: 0 },
-    { dia: 227, nivel: 0 },
+    { dia: 227, nivel: 4 },
     { dia: 228, nivel: 4 },
     { dia: 229, nivel: 0 },
 
@@ -266,7 +266,7 @@ var diasDelAño = [
     { dia: 231, nivel: 0 },
     { dia: 232, nivel: 0 },
     { dia: 233, nivel: 0 },
-    { dia: 234, nivel: 0 },
+    { dia: 234, nivel: 4 },
     { dia: 235, nivel: 4 },
     { dia: 236, nivel: 0 },
     { dia: 237, nivel: 0 },
@@ -275,14 +275,14 @@ var diasDelAño = [
 
     /* JULIO */
     { dia: 240, nivel: 0 },
-    { dia: 241, nivel: 0 },
+    { dia: 241, nivel: 4 },
     { dia: 242, nivel: 4 },
     { dia: 243, nivel: 0 },
     { dia: 244, nivel: 0 },
     { dia: 245, nivel: 0 },
     { dia: 246, nivel: 0 },
     { dia: 247, nivel: 0 },
-    { dia: 248, nivel: 0 },
+    { dia: 248, nivel: 4 },
     { dia: 249, nivel: 4 },
 
     { dia: 250, nivel: 0 },
@@ -290,7 +290,7 @@ var diasDelAño = [
     { dia: 252, nivel: 0 },
     { dia: 253, nivel: 0 },
     { dia: 254, nivel: 0 },
-    { dia: 255, nivel: 0 },
+    { dia: 255, nivel: 4 },
     { dia: 256, nivel: 4 },
     { dia: 257, nivel: 0 },
     { dia: 258, nivel: 0 },
@@ -298,7 +298,7 @@ var diasDelAño = [
 
     { dia: 260, nivel: 0 },
     { dia: 261, nivel: 0 },
-    { dia: 262, nivel: 0 },
+    { dia: 262, nivel: 4 },
     { dia: 263, nivel: 4 },
     { dia: 264, nivel: 0 },
     { dia: 265, nivel: 0 },
@@ -306,9 +306,6 @@ var diasDelAño = [
     { dia: 267, nivel: 0 },
     { dia: 268, nivel: 0 },
     { dia: 269, nivel: 0 },
-
-
-
 
   ];
   
